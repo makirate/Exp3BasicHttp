9762cb7cad6257c1826423b4c235e2368d314a1f
epic of remnant <e185708@ie.u-ryukyu.ac.jp>
2020-06-03 20:39:08 +0900

    Initial commit

--- Server/Server.py
+++ Server/Server.py
@@ -2,11 +2,13 @@
 
 import socket
 
+from http import HTTPStatus
+
 # AF = IPv4 という意味
 # TCP/IP の場合は、SOCK_STREAM を使う
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # IPアドレスとポートを指定
-    s.bind(('127.0.0.1', 50007))
+    s.bind(('0.0.0.0', 50007))
     # 1 接続
     s.listen(1)
     # connection するまで待つ
