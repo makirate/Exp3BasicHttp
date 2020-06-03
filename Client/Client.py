b8177da43aeb74d1e356b46fd69b72a591909a00
epic of remnant <e185708@ie.u-ryukyu.ac.jp>
2020-06-03 20:42:25 +0900

    change address

--- Client/Client.py
+++ Client/Client.py
@@ -2,7 +2,7 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
-    s.connect(('127.0.0.1', 50007))
+    s.connect(('54.65.248.63', 50007))
     # サーバにメッセージを送る
     s.sendall(b'hello')
     # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
